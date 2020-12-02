d4f6488e86f87b2f7511804aab7fde2ca728b2f4
DESKTOP-GOO643T\Александра <alieva.alexandra@gmail.com>
2020-12-02 18:03:07 +0300

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function()
+{
+    var mySrc = myImage.getAttribute ('src');
+    if (mySrc === 'images/20191122_16.jpg')
+{
+    myImage.setAttribute ('src', 'images/unnamed.jpg');
+
+}
+else
+{
+    myImage.setAttribute ('src', 'images/20191122_16.jpg');
+}
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h6');
+
+function setUserName ()
+{
+    var myName = prompt ('Пожалуйста, введите имя.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Добро пожаловать,' + myName;
+
+}
+
+if(!localStorage.getItem('name'))
+{
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Добро пожаловать снова,' + storedName;
+}
+
+myButton.onclick = function ()
+{
+    setUserName();
+}
